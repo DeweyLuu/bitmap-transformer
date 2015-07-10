45ef8158f6916f65b649b8613d80d59ba6efedb4
Dewey Luu <deweyluu@gmail.com>
2015-07-10 11:37:39 -0700

    added black and white

--- bitmap.js
+++ bitmap.js
@@ -4,31 +4,24 @@ var fs = require('fs');
 var buff = require('buffer');
 
 var buf = new Buffer(fs.readFileSync('./palette-bitmap.bmp'));
-var buff = fs.readFileSync('./palette-bitmap.bmp');
+var buff = fs.readFileSync('./non-palette-bitmap.bmp');
 //buf.toString('ascii', 0, 2);
-var bufff = buff.toString('ascii');
-//console.log(bufff);
-
-var adjust = buf.toJSON().toString('ascii');
-var jayson = buf.toJSON().data;
-//console.log(jayson.toString());
-//console.log(buf.toString('utf-8', 14, 320));
-//console.log(buf.readInt32LE());
-//console.log(buf.readUInt8(buf));
 
 fs.readFile('./palette-bitmap.bmp', function(err, data) {
-	//console.log(data.length);
-	//console.log(data.toString('ascii', 0, 2));
-	//console.log(data.toString('ascii', 80, 112));
+	//console.log(data.readUInt32LE(10));
+	for (var i = 54; i < data.length; i++) {
+		data.writeUInt8(0, i);
+	}
+	fs.writeFile('test2.bmp', data);
 });
 
 fs.readFile('./non-palette-bitmap.bmp', function(err, data) {
-	console.log(data.length);
-	console.log(data);
-	console.log(data.toString('ascii', 0, 2));
-	console.log(data.toString('utf-8', 2, 32));
-	console.log(data.toString('utf-8', 32, 80));
-	console.log(data.toJSON().data.toString());
+	for (var i = 54; i < data.length; i++) {
 
+		data.writeUInt8(255, i);
+	}
+	//console.log(data.readUInt32LE(10));
+	//console.log(data.readUInt8(14));
+	fs.writeFile('test.bmp', data);
 });
 
