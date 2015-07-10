9c192e22265fda0ee642e54fc65b8b5a89beca26
Dewey Luu <deweyluu@gmail.com>
2015-07-10 15:56:18 -0700

    playing around

--- bitmap.js
+++ bitmap.js
@@ -5,23 +5,36 @@ var buff = require('buffer');
 
 var buf = new Buffer(fs.readFileSync('./palette-bitmap.bmp'));
 var buff = fs.readFileSync('./non-palette-bitmap.bmp');
-//buf.toString('ascii', 0, 2);
+console.log(buf.readUInt32LE(10));
+console.log(buff.readUInt32LE(10));
 
+function notPalette(data) {
+	var starting = data.readUInt32LE(10);
+	for (var i = starting; i < data.length; i++) {
+		var random = Math.ceil(Math.random() *255);
+		data.writeUInt8(random, i);
+	}
+	fs.writeFile('test.bmp', data);
+	return 'bitmap randomized';
+};
+
+notPalette(buff);
+/*
 fs.readFile('./palette-bitmap.bmp', function(err, data) {
-	//console.log(data.readUInt32LE(10));
-	for (var i = 54; i < data.length; i++) {
-		data.writeUInt8(0, i);
+	var starting = data.readUInt32LE(10);
+	for (var i = starting; i < data.length; i+=4) {
+		//data.writeUInt8(255 - data.readUInt8(i), i);
+		data.writeUInt8(255 - data.readUInt8(i), i);
 	}
 	fs.writeFile('test2.bmp', data);
 });
 
 fs.readFile('./non-palette-bitmap.bmp', function(err, data) {
-	for (var i = 54; i < data.length; i++) {
-
-		data.writeUInt8(255, i);
+	var starting = data.readUInt32LE(10);
+	for (var i = starting; i < data.length; i++) {
+		var random = Math.ceil(Math.random() * 255);
+		data.writeUInt8(random, i);
 	}
-	//console.log(data.readUInt32LE(10));
-	//console.log(data.readUInt8(14));
 	fs.writeFile('test.bmp', data);
 });
-
+*/
--- test/test.js
+++ test/test.js
@@ -1,2 +1,12 @@
 //this is for tests!//
-var expect = require('chai');
+var expect = require('chai').expect;
+var bitmap = require('../bitmap.js');
+var fs = require('fs');
+
+var buff = fs.readFileSync('./non-palette-bitmap.bmp');
+
+describe('bitmap', function() {
+	it('will display rainbow colors', function() {
+		expect(notPalette(buff)).to.equal('bitmap randomized');
+	});
+});
